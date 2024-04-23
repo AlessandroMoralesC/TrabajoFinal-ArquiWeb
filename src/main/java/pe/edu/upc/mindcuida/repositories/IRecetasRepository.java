0392a884baecdb31a48d414e4c1d4bb9a043005f
J0elCLC <u202117271@upc.edu.pe>
2024-04-22 19:01:33 -0500

    Version 2.0

--- src/main/java/pe/edu/upc/mindcuida/repositories/IRecetasRepository.java
+++ src/main/java/pe/edu/upc/mindcuida/repositories/IRecetasRepository.java
@@ -1,7 +1,8 @@
 package pe.edu.upc.mindcuida.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pe.edu.upc.mindcuida.entities.Recetas;
-
+@Repository
 public interface IRecetasRepository extends JpaRepository<Recetas,Integer> {
 }
