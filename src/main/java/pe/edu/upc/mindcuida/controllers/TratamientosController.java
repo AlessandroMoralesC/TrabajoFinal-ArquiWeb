2e33e8d292b87d50cfedd46e99378298722ce0b8
J0elCLC <u202117271@upc.edu.pe>
2024-04-22 19:00:33 -0500

    Version 2.0

--- src/main/java/pe/edu/upc/mindcuida/controllers/TratamientosController.java
+++ src/main/java/pe/edu/upc/mindcuida/controllers/TratamientosController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("tratamientos")
+@RequestMapping("/tratamientos")
 public class TratamientosController {
     @Autowired
     private ITratamientosService trS;
--- src/main/java/pe/edu/upc/mindcuida/repositories/IMensajesRepository.java
+++ src/main/java/pe/edu/upc/mindcuida/repositories/IMensajesRepository.java
@@ -1,7 +1,8 @@
 package pe.edu.upc.mindcuida.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pe.edu.upc.mindcuida.entities.Mensajes;
-
+@Repository
 public interface IMensajesRepository extends JpaRepository<Mensajes,Integer> {
 }
