b9174783d15f7d9f74b57c5d2b9fd1a83eeb664d
J0elCLC <u202117271@upc.edu.pe>
2024-04-22 19:01:33 -0500

    Version 2.0

--- src/main/java/pe/edu/upc/mindcuida/repositories/ITipoMaterialRepository.java
+++ src/main/java/pe/edu/upc/mindcuida/repositories/ITipoMaterialRepository.java
@@ -1,7 +1,8 @@
 package pe.edu.upc.mindcuida.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pe.edu.upc.mindcuida.entities.TipoMaterial;
-
+@Repository
 public interface ITipoMaterialRepository extends JpaRepository<TipoMaterial, Integer> {
 }
