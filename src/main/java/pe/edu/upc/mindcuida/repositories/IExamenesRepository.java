6326522076b64fd518f4ec4d7d15412f1d3add53
J0elCLC <u202117271@upc.edu.pe>
2024-04-22 19:00:33 -0500

    Version 2.0

--- src/main/java/pe/edu/upc/mindcuida/repositories/IExamenesRepository.java
+++ src/main/java/pe/edu/upc/mindcuida/repositories/IExamenesRepository.java
@@ -1,7 +1,9 @@
 package pe.edu.upc.mindcuida.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pe.edu.upc.mindcuida.entities.Examenes;
+@Repository
 
 public interface IExamenesRepository extends JpaRepository<Examenes,Integer> {
 }
