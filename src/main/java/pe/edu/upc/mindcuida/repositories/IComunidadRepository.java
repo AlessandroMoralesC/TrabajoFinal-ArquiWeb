104d35128311e7049d17455eae38a825a8b1338a
GGstorm99 <116691271+GGstorm99@users.noreply.github.com>
2024-04-26 19:05:21 -0500

    Update IComunidadRepository.java
    adicion querys listar experiencias y recomendaciones

--- src/main/java/pe/edu/upc/mindcuida/repositories/IComunidadRepository.java
+++ src/main/java/pe/edu/upc/mindcuida/repositories/IComunidadRepository.java
@@ -6,4 +6,13 @@ import pe.edu.upc.mindcuida.entities.Comunidad;
 
 @Repository
 public interface IComunidadRepository extends JpaRepository<Comunidad,Integer> {
+      @Query(value = "SELECT experiencias_comunidad\n" +
+            "FROM comunidad\n" +
+            "WHERE experiencias_comunidad IS NOT NULL;", nativeQuery = true)
+    List<String[]> identificaExp();
+
+    @Query(value = "SELECT recomendaciones_comunidad\n" +
+            "FROM comunidad\n" +
+            "WHERE recomendaciones_comunidad IS NOT NULL;", nativeQuery = true)
+    List<String[]> identificaRec();
 }
