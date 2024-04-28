fb59d448b717bb1f4c510ae1424b47cc9e22b634
joooelpl <137342108+joooelpl@users.noreply.github.com>
2024-04-27 20:08:17 -0500

    Añadir atributos en la clase Users entities y crear dtos

--- src/main/java/pe/edu/upc/mindcuida/dtos/UserDTO.java
+++ src/main/java/pe/edu/upc/mindcuida/dtos/UserDTO.java
@@ -1,8 +1,17 @@
 package pe.edu.upc.mindcuida.dtos;
 
+import jakarta.persistence.Column;
+
+import java.time.LocalDate;
+
 public class UserDTO {
     private Long id;
-
+    private String nombreUsers;
+    private String apellidoUsers;
+    private LocalDate fechanaciemientoUsers;
+    private String telefonoUsers;
+    private String correoUsers;
+    private String especialidadUsers;
     private String username;
     private String password;
     private Boolean enabled;
@@ -37,5 +46,53 @@ public class UserDTO {
 
     public void setEnabled(Boolean enabled) {
         this.enabled = enabled;
+    }
+
+    public String getNombreUsers() {
+        return nombreUsers;
+    }
+
+    public void setNombreUsers(String nombreUsers) {
+        this.nombreUsers = nombreUsers;
+    }
+
+    public String getApellidoUsers() {
+        return apellidoUsers;
+    }
+
+    public void setApellidoUsers(String apellidoUsers) {
+        this.apellidoUsers = apellidoUsers;
+    }
+
+    public LocalDate getFechanaciemientoUsers() {
+        return fechanaciemientoUsers;
+    }
+
+    public void setFechanaciemientoUsers(LocalDate fechanaciemientoUsers) {
+        this.fechanaciemientoUsers = fechanaciemientoUsers;
+    }
+
+    public String getTelefonoUsers() {
+        return telefonoUsers;
+    }
+
+    public void setTelefonoUsers(String telefonoUsers) {
+        this.telefonoUsers = telefonoUsers;
+    }
+
+    public String getCorreoUsers() {
+        return correoUsers;
+    }
+
+    public void setCorreoUsers(String correoUsers) {
+        this.correoUsers = correoUsers;
+    }
+
+    public String getEspecialidadUsers() {
+        return especialidadUsers;
+    }
+
+    public void setEspecialidadUsers(String especialidadUsers) {
+        this.especialidadUsers = especialidadUsers;
     }
 }
--- src/main/java/pe/edu/upc/mindcuida/entities/Users.java
+++ src/main/java/pe/edu/upc/mindcuida/entities/Users.java
@@ -3,6 +3,7 @@ package pe.edu.upc.mindcuida.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 import java.util.List;
 
 @Entity
@@ -12,15 +13,43 @@ public class Users implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "nombreUsers",nullable = false,length = 200)
+    private String nombreUsers;
+    @Column(name = "apellidoUsers",nullable = false,length = 200)
+    private String apellidoUsers;
+    @Column(name = "fechanacimientoUsers",nullable = false)
+    private LocalDate fechanaciemientoUsers;
+    @Column(name = "telefonoUsers",nullable = false,length = 11)
+    private String telefonoUsers;
+    @Column(name = "correoUsers",nullable = false,length = 200)
+    private String correoUsers;
+    @Column(name = "especialidadUsers",nullable = true,length = 200)
+    private String especialidadUsers;
 
     @Column(length = 30, unique = true)
     private String username;
     @Column(length = 200)
     private String password;
+
+
     private Boolean enabled;
     @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @JoinColumn(name = "user_id")
     private List<Role> roles;
+
+    public Users() {
+    }
+
+    public Users(Long id, String nombreUsers, String apellidoUsers, LocalDate fechanaciemientoUsers, String telefonoUsers, String correoUsers, String especialidadUsers, List<Role> roles) {
+        this.id = id;
+        this.nombreUsers = nombreUsers;
+        this.apellidoUsers = apellidoUsers;
+        this.fechanaciemientoUsers = fechanaciemientoUsers;
+        this.telefonoUsers = telefonoUsers;
+        this.correoUsers = correoUsers;
+        this.especialidadUsers = especialidadUsers;
+        this.roles = roles;
+    }
 
     public Long getId() {
         return id;
@@ -62,4 +91,51 @@ public class Users implements Serializable {
         this.roles = roles;
     }
 
+    public String getNombreUsers() {
+        return nombreUsers;
+    }
+
+    public void setNombreUsers(String nombreUsers) {
+        this.nombreUsers = nombreUsers;
+    }
+
+    public String getApellidoUsers() {
+        return apellidoUsers;
+    }
+
+    public void setApellidoUsers(String apellidoUsers) {
+        this.apellidoUsers = apellidoUsers;
+    }
+
+    public LocalDate getFechanaciemientoUsers() {
+        return fechanaciemientoUsers;
+    }
+
+    public void setFechanaciemientoUsers(LocalDate fechanaciemientoUsers) {
+        this.fechanaciemientoUsers = fechanaciemientoUsers;
+    }
+
+    public String getTelefonoUsers() {
+        return telefonoUsers;
+    }
+
+    public void setTelefonoUsers(String telefonoUsers) {
+        this.telefonoUsers = telefonoUsers;
+    }
+
+    public String getCorreoUsers() {
+        return correoUsers;
+    }
+
+    public void setCorreoUsers(String correoUsers) {
+        this.correoUsers = correoUsers;
+    }
+
+    public String getEspecialidadUsers() {
+        return especialidadUsers;
+    }
+
+    public void setEspecialidadUsers(String especialidadUsers) {
+        this.especialidadUsers = especialidadUsers;
+    }
 }
