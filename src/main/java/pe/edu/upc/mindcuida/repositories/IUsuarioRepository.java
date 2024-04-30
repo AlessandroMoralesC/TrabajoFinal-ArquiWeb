308b6b034808c04f0f1f833c7ccadf8958f5c237
J0elCLC <u202117271@upc.edu.pe>
2024-04-29 20:52:49 -0500

    Correction - Comunidad

--- src/main/java/pe/edu/upc/mindcuida/repositories/IUsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package pe.edu.upc.mindcuida.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pe.edu.upc.mindcuida.entities.Usuario;
-
-public interface IUsuarioRepository extends JpaRepository<Usuario,Integer> {
-}
